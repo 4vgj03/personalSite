976d252a166caaf1bafaf12503178f5f67426e94
Joseph Mather <jmather@tomorrowenergy.com>
2024-05-21 12:10:07 -0500

    v4 push for contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -44,7 +44,7 @@ const Contact = () => {
 
   return (
     <div>
-      <h1>Contact 5/20/2024 v3</h1>
+      <h1>Contact 5/21/2024 v4</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label>Name:</label>
