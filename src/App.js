4bdaff5ff2b5f300be54b94f5b8fb02e7595f4ce
Joseph Mather <jmather@tomorrowenergy.com>
2024-05-22 10:07:05 -0500

    reverting app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/Header'; // Import Header from separate file
 import Home from './components/Home';
 import Resume from './components/Resume';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import './App.css';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router basename="/">
-      <Header />
+    <Router>
+      <Header /> {/* Render the Header component */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer />
+      <Footer /> {/* Render the Footer component */}
     </Router>
   );
 }
-
-export default App;
