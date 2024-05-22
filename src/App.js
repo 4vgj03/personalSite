58f0e10b09ba1571a30b1523478477c87aae40de
Joseph Mather <jmather@tomorrowenergy.com>
2024-05-22 10:26:19 -0500

    trying to implement hook router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header'; // Import Header from separate file
+import { useRoutes } from 'hookrouter';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import Resume from './components/Resume';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import './App.css';
-import Footer from './components/Footer';
+import NotFoundPage from './components/NotFoundPage';
+
+const routes = {
+    '/': () => <Home />,
+    '/resume': () => <Resume />,
+    '/projects': () => <Projects />,
+    '/contact': () => <Contact />,
+};
 
 function App() {
-  return (
-    <Router>
-      <Header /> {/* Render the Header component */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer /> {/* Render the Footer component */}
-    </Router>
-  );
+    const routeResult = useRoutes(routes);
+
+    return (
+        <>
+            <Header />
+            {routeResult || <NotFoundPage />}
+            <Footer />
+        </>
+    );
 }
 
 export default App;
