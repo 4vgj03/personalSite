5e90afc6939fc0980a983251121a3471108539c9
Joseph Mather <jmather@tomorrowenergy.com>
2024-05-22 10:00:03 -0500

    adding browser route options to access subpages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header'; // Import Header from separate file
+import Header from './components/Header';
 import Home from './components/Home';
 import Resume from './components/Resume';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import './App.css';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <Header /> {/* Render the Header component */}
+    <Router basename="/">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer /> {/* Render the Footer component */}
+      <Footer />
     </Router>
   );
 }
