80a2137f5cc75dc9b9be1a900c381c1c838a852c
Joseph Mather <jmather@tomorrowenergy.com>
2024-05-31 17:00:15 -0500

    mobile styling for home page

--- src/components/TopContainer.js
+++ src/components/TopContainer.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap is imported
 import './TopContainer.css';
 
 const TopContainer = () => {
   return (
     <div className="top-container text-center">
-      <img className="cloud-top" src="/images/floating-clouds-2.gif" alt="cloudimage" />
+      <img className="cloud-top img-fluid" src="/images/floating-clouds-2.gif" alt="cloudimage" />
       <h1>hi, I'm Joe</h1>
       <h2>I like to code</h2>
-      <img src="/images/mountain.png" alt="mountainimage" />
-      <img className="cloud-bottom" src="/images/floating-clouds-2.gif" alt="cloudimage" />
+      <img className="mountain img-fluid" src="/images/mountain.png" alt="mountainimage" />
+      <img className="cloud-bottom img-fluid" src="/images/floating-clouds-2.gif" alt="cloudimage" />
     </div>
   );
 };
