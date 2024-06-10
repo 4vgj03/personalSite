337aa6be58e6b2d82069fc0cb3413862bcc58e09
Joseph Mather <jmather@tomorrowenergy.com>
2024-06-10 16:54:00 -0500

    mostly resume page updates

--- src/components/BottomContainer.js
+++ src/components/BottomContainer.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './BottomContainer.css';
 
 const BottomContainer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="bottom-container text-center gradient-background">
       <a className="footer-link" href="https://www.linkedin.com/">LinkedIn</a>
       <a className="footer-link" href="https://twitter.com/">Twitter</a>
       <a className="footer-link" href="https://www.appbrewery.co/">Website</a>
-      <p className="copyright">© Joseph Mather.</p>
+      <p className="copyright">©{currentYear} Joseph Mather</p>
     </div>
   );
 };
